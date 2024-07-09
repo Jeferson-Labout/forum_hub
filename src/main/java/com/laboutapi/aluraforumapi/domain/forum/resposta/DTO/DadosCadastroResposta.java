50cd000f8236f8d36c8cc2f70784fc725b1ae0f2
jlabout <jefson1989@gmail.com>
2024-07-09 15:50:07 -0300

    alateração codigo

--- src/main/java/com/laboutapi/aluraforumapi/domain/forum/resposta/DTO/DadosCadastroResposta.java
+++ src/main/java/com/laboutapi/aluraforumapi/domain/forum/resposta/DTO/DadosCadastroResposta.java
@@ -1,7 +1,6 @@
 package com.laboutapi.aluraforumapi.domain.forum.resposta.DTO;
 
-import com.laboutapi.aluraforumapi.domain.forum.topico.Topico;
-import com.laboutapi.aluraforumapi.domain.user.Usuario;
+
 
 public record DadosCadastroResposta(String mensagem, Long autor, Long topico) {
 }
